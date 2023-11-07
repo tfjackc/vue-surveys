97bcbd54d0bc3f0c5182e6b3cdb28d05cd658272
tfjackc <jack.colpitt@crookcountyor.gov>
2023-11-07 11:57:07 -0800

    working on expansion panel

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -16,7 +16,13 @@
                         <MapComponent />
                     </v-col>
                 </v-row>
+          <v-expansion-panels v-if="filteredData.length > 0" flat height="92">
+            <v-expansion-panel title="Data Table">
+              <v-expansion-panel-text>
                 <TableComponent />
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-main>
     </v-app>
 </template>
@@ -25,5 +31,9 @@ import MapComponent from "@/components/MapComponent.vue";
 import LayerList from "@/components/LayerList.vue";
 import Search from "@/components/Search.vue";
 import TableComponent from "@/components/TableComponent.vue";
+import { useMappingStore } from "@/store/mapping";
+import {storeToRefs} from "pinia";
+const mapping_store = useMappingStore()
+const { filteredData } = storeToRefs(mapping_store)
 </script>
 
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
-<!--    <v-list-item>-->
-<!--      <v-select label="Filter Search Criteria" :items="selection_criteria" variant="solo-filled"></v-select>-->
-<!--    </v-list-item>-->
+    <v-list-item>
+      <v-select label="Filter Search Criteria" :items="selection_criteria" variant="solo-filled"></v-select>
+    </v-list-item>
     <v-list-item>
       <v-form v-model="form"
               @submit.prevent="mapping_store.onSubmit()">
@@ -29,16 +29,17 @@
 <script setup lang="ts">
 import { useMappingStore } from "@/store/mapping";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 const mapping_store = useMappingStore()
 const { form, loading, searchedValue } = storeToRefs(mapping_store)
-// const selection_criteria = ref([
-//   'Survey Numbers',
-//   'Partition Plats',
-//   'Township/Ranges',
-//   'Subdivisions',
-//   'Prepared For',
-//   'Prepared By'
-// ])
+const selection_criteria = ref([
+  'Survey Numbers',
+  'Partition Plats',
+  'Township/Ranges',
+  'Subdivisions',
+  'Prepared For',
+  'Prepared By'
+])
 
 function required (v: any) {
   return !!v || 'Field is required'
