77786cf2c5e14340c1d0a8889c1b6e182770e0dd
tfjackc <jack.colpitt@crookcountyor.gov>
2023-11-07 09:51:36 -0800

    disclaimer added

---
+++ src/components/Disclaimer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col cols="auto">
+    <v-dialog
+      transition="dialog-top-transition"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" color="primary" size="small" variant="elevated">Disclaimer</v-btn>
+      </template>
+      <template v-slot:default="{ isActive }">
+        <v-card>
+          <v-toolbar
+            color="primary"
+            title="Disclaimer"
+            size="large"
+          ></v-toolbar>
+          <v-card-text>
+            <div class="text-small pa-12"><strong><h3>Contact surveyor as not all surveys are present in this application (541) 420-8057.</h3></strong> <br />
+              Crook County makes no warranty of any kind, expressed or implied, including any warranty of merchantability, fitness for a particular purpose, or any other matter. The County is not responsible for possible errors, omissions, misuse, or misinterpretation. County digital information is prepared for reference purposes only and should not be used, and is not intended for, survey or engineering purposes or the authoritative and/or precise location of boundaries, fixed human works, and/or the shape and contour of the earth. No representation is made concerning the legal status of any apparent route of access identified in digital or hardcopy mapping of geospatial information or data. Data from the Crook County Assessor’s Office may not be current. Data is updated as schedules and resources permit. Please notify Crook County GIS of any errors (541) 416-3930.</div>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
+              variant="elevated"
+              @click="isActive.value = false"
+              color="primary"
+            >Agree</v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </v-col>
+</template>
+
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app-bar flat height="92">
-      <v-icon size="80">
-        <v-img
-          src="src/assets/cc_logo.png"
-        ></v-img>
-      </v-icon>
-        <h2>Survey Research Tool</h2>
-      <v-spacer></v-spacer>
-      <v-responsive max-width="160">
-      </v-responsive>
+    <v-icon size="80">
+      <v-img src="src/assets/cc_logo.png"></v-img>
+    </v-icon>
+    <h2>Survey Research Tool</h2>
+    <v-btn href="https://geo.co.crook.or.us/surveys_legacy/" color="primary" size="small" variant="elevated">Legacy Survey Search Tool</v-btn>
+    <v-spacer></v-spacer>
+
+    <Disclaimer />
   </v-app-bar>
 </template>
 <script lang="ts" setup>
+import Disclaimer from "@/components/Disclaimer.vue";
 </script>
